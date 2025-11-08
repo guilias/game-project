e63e2eeafa4673177fbd14ebe3d924138f4f8304
guilias <guilhermeeliasb@gmail.com>
2025-11-08 14:42:39 -0300

    Sistema de pontuação adicionado e funcionado; Movimentação do inimigo refinada: Agora ele se move de forma mais errática, permitindo que o jogador escape com maior facilidade; Protótipo de tela de Game Over e reinicialização do jogo.

--- game/classes_e_objetos.js
+++ game/classes_e_objetos.js
@@ -19,6 +19,8 @@ let player = {
     velVertical: 0,
     forcaPulo: -4,
     colisao: false,
+    vivo: true,
+    pontuacao: 0,
     carregarVisual(linkVisual){
         this.visual = new Image(this.largura, this.altura);
         this.visual.src = linkVisual;
@@ -50,6 +52,18 @@ class Inimigo{
 
         this.x -= (distanciaX/distanciaTotal) * this.velocidade;
         this.y -= (distanciaY/distanciaTotal) * this.velocidade;
+
+        let ruidoMovFantasmaX = ((Math.random() - 0.5) * 2) * 3 
+        let ruidoMovFantasmaY = ((Math.random() - 0.5) * 2) * 10
+        this.x += ruidoMovFantasmaX;
+        this.y += ruidoMovFantasmaY;
+    }
+    dano(){
+    if (player.x < this.x + this.largura &&
+        player.x + player.largura > this.x &&
+        player.y < this.y + this.altura &&
+        player.y + player.altura > this.y)
+        {player.pv -= 1;}
     }
 }
 
@@ -171,22 +185,34 @@ class Item {
         player.y + player.altura > this.y)
             {player.pv -= 1;}
     }
+    pontuar(){
+    if (this.coletado == false &&
+        player.x < this.x + this.largura &&
+        player.x + player.largura > this.x &&
+        player.y < this.y + this.altura &&
+        player.y + player.altura > this.y)
+            {
+            player.pontuacao += 1;
+            this.coletado = true
+            }
+    }
 }
 
 class Texto {
-    constructor(tamanho, fonte, cor, x, y, mensagem){
+    constructor(tamanho, fonte, cor, x, y, alinhamento, mensagem){
         this.fonte = fonte;
         this.tamanho = tamanho;
         this.cor = cor;
         this.x = x;
         this.y = y;
+        this.alinhamento = alinhamento;
         this.mensagem = mensagem;
     }
     desenha(){
         ctx.beginPath()
         ctx.font = `${this.tamanho} ${this.fonte}`;
         ctx.fillStyle = this.cor;
-        ctx.textAlign = 'center';
+        ctx.textAlign = this.alinhamento;
         ctx.fillText(this.mensagem, this.x, this.y);
     }
 };
@@ -209,12 +235,20 @@ let plataforma = new Objeto("gray", null, canvas.width / 2, 500, 400, 10, false)
 let dano01 = new Objeto("purple", null, 1000, canvas.height - 20, 100, 10, true);
 
 //ITEM:
+//curas
 let cura01 = new Item("green", 400, 400, 48, 48, null, false)
 let cura02 = new Item("green", 50, 400, 48, 48, null, false)
 
-//TEXTO:
-let msgJogadorNoChao = new Texto("50px", "Arial", "black", canvas.width / 2, canvas.height / 2, "O jogador está no chão.")
+let documento01 = new Item("gray", 500, 500, 48, 48, null, false)
 
+//TEXTO:
+let msgJogadorNoChao = new Texto("50px", "Arial", "black", canvas.width / 2, canvas.height / 2, "center", "O jogador está no chão.")
+
+
+//INTERFACE
+let vermelho = new Objeto("red", null, 0, 0, canvas.width, canvas.height, false)
+let msgGameOver = new Texto("50px", "Arial", "black", canvas.width/2, canvas.height/2, "center", "Você morreu.")
+let msgReiniciarJogo = new Texto("25px", "Arial", "black", canvas.width/2, canvas.height - canvas.height/3, "center", "Pressione a tecla R para tentar novamente.")
 
 
 //Mapeamento de teclas: adiciona o "sinal" e cria variável para rastrear teclas pressionadas
@@ -232,40 +266,29 @@ function loopAnimacao(){
     
     //--- MOVIMENTAÇÃO: ---
 
-    //baixo, esquerda e direita:
-    if(teclasPressionadas['ArrowDown'] || teclasPressionadas['s'])
-        {player.y += player.velocidade;}
-    if(teclasPressionadas['ArrowLeft'] || teclasPressionadas['a'])
-        {player.x -= player.velocidade;}
-    if(teclasPressionadas['ArrowRight'] || teclasPressionadas['d'])
-        {player.x += player.velocidade;}
 
-    //pulo
-    if((teclasPressionadas['ArrowUp'] || teclasPressionadas['w']) && player.noChao == true){
-        player.velVertical = player.forcaPulo; //basicamente, é o impulso do pulo.
-        player.noChao = false;
+    //antes de mais nada, chegar se o jogo está vivo:
+    if (player.vivo == true){
+    //baixo, esquerda e direita:
+        if(teclasPressionadas['ArrowDown'] || teclasPressionadas['s'])
+            {player.y += player.velocidade;}
+        if(teclasPressionadas['ArrowLeft'] || teclasPressionadas['a'])
+            {player.x -= player.velocidade;}
+        if(teclasPressionadas['ArrowRight'] || teclasPressionadas['d'])
+            {player.x += player.velocidade;}
+
+        //pulo
+        if((teclasPressionadas['ArrowUp'] || teclasPressionadas['w']) && player.noChao == true){
+            player.velVertical = player.forcaPulo; //basicamente, é o impulso do pulo.
+            player.noChao = false;
+        }
+        playerPlaceholder.x = player.x
+        playerPlaceholder.y = player.y
     }
-    playerPlaceholder.x = player.x
-    playerPlaceholder.y = player.y
 
     //"física"
     player.velVertical += 0.15 // a velocidade de queda aumenta a cada frame
     player.y += player.velVertical * gravidade // incrementa os valores
-
-
-    //--- INTERFACE: ---
-
-    //Pontos de vida:
-    
-    let msgPv = new Texto("20px", "Arial", "black", 100, 50, "Pontos de vida: " + player.pv)
-    msgPv.desenha()
-    if (player.pv > player.pvMax){
-            player.pv = player.pvMax;
-        }
-        if (player.pv < 1){
-            player.pv = 0;
-        }
-    
 
     
     // -- DEMAIS FUNÇÕES... --
@@ -297,10 +320,53 @@ function loopAnimacao(){
     cura02.curar()
     cura02.carregarVisual("img/pocao.png")
 
+    //Documentos:
+    documento01.pontuar()
+    documento01.carregarVisual("img/documento.png")
+
+
     //FANTASMA
     //nao funciona
     fantasma.desenha()
     fantasma.seguirJogador()
+    fantasma.dano();
+
+    if (player.pv == 0){
+        vermelho.desenha()
+        msgGameOver.desenha()
+    }
+
+    // --- INTERFACE ---
+
+    //Pontos de VIda:
+    let msgPv = new Texto("20px", "Arial", "black", 30, 50, "left", "Pontos de vida: " + player.pv)
+
+    msgPv.desenha()
+    if (player.pv > player.pvMax){
+            player.pv = player.pvMax;
+        }
+        if (player.pv < 1){
+            player.pv = 0;
+        if (teclasPressionadas['r']){ //Caso o jogador esteja morto, permite reiniciar o jogo.
+            location.reload();
+        }
+        }
+
+    //Pontuação
+    let msgPontuacao = new Texto("20px", "Arial", "black", 30, 100, "left", "Documentos coletados:  " + player.pontuacao)
+    msgPontuacao.desenha()
+
+
+    if(teclasPressionadas['x']){window.location.href = "https://www.example.com/new-page.html";}
+
+    //Game Over (precisa estar por último)
+    if (player.pv == 0){
+        player.vivo = false;
+        vermelho.desenha()
+        msgGameOver.desenha()
+        msgReiniciarJogo.desenha()
+    }
+
 
     requestAnimationFrame(loopAnimacao);
 }
